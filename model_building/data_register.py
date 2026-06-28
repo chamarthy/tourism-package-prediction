76d10ce106ba3adff6de6d7150f20792d808be62
chamarthy <chamarthy.raju@gmail.com>
2026-06-28 08:31:07 +0000

    add model building file.

---
+++ model_building/data_register.py
@@ -0,0 +1,27 @@
+# Import necessary libraries from huggingface_hub
+from huggingface_hub.utils import RepositoryNotFoundError
+from huggingface_hub import HfApi, create_repo
+import os
+
+# Define repository details
+repo_id = "schamart/tourism-wellness"
+repo_type = "dataset"
+
+# Initialize API client using the Hugging Face token from environment variables
+api = HfApi(token=os.getenv("HF_TKN"))
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
+# Step 2: Upload the data folder to the repo
+api.upload_folder(
+    folder_path="tourism-wellness/data",
+    repo_id=repo_id,
+    repo_type=repo_type,
+)
